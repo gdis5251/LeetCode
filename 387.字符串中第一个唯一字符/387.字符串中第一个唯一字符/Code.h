e17809553a219cd4ca1752885b24b15d34114e11
gdis5251 <841784596@qq.com>
2019-03-19 15:08:00 +0800

    [郭文峰][更新][2019/3/19] 优化了空间复杂度，使空间复杂度为O(1)

--- 387.字符串中第一个唯一字符/387.字符串中第一个唯一字符/Code.h
+++ 387.字符串中第一个唯一字符/387.字符串中第一个唯一字符/Code.h
@@ -6,20 +6,16 @@ public:
 		if (s.empty())
 			return -1;
 
-		std::string book(s.size(), '\0');
 
 		int pos = 0;
 		for (pos; pos < s.size(); ++pos)
 		{
-			if (s.find(s[pos], pos + 1) != -1)
+			if (s.find(s[pos], pos + 1) == -1)
 			{
-				book.push_back(s[pos]);
-			}
-			else
-			{
-				if (book.find(s[pos]) == -1)
+				if (s.find(s[pos]) == pos)
 					return pos;
 			}
+
 		}
 
 		return -1;
