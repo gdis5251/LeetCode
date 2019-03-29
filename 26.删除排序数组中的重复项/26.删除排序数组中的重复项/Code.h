30368daee3a9419087c45c660ea1a0636439b16a
gdis5251 <841784596@qq.com>
2019-03-29 14:48:23 +0800

    [郭文峰][更新][2019/3/29] 首先优化了C语言版本的代码量，其次增加了C++版本的使用vector容器的接口的做法

--- 26.删除排序数组中的重复项/26.删除排序数组中的重复项/Code.h
+++ 26.删除排序数组中的重复项/26.删除排序数组中的重复项/Code.h
@@ -1,21 +1,52 @@
-int removeDuplicates(int* nums, int numsSize) {
-	if (numsSize == 0)
-		return 0;
 
-	int cur = 0;
-	int pre = 0;
+//c语言做法
 
-	while (cur < numsSize)
-	{
-		if (nums[cur] == nums[pre])
-			cur++;
-		else
+//int removeDuplicates(int* nums, int numsSize) {
+//	if (numsSize == 0)
+//		return 0;
+//
+//	int pre = 0;
+//	int cur = 1;
+//
+//	while (cur < numsSize)
+//	{
+//		if (nums[pre] != nums[cur])
+//		{
+//			nums[++pre] = nums[cur++];
+//		}
+//		else
+//		{
+//			cur++;
+//		}
+//	}
+//
+//	return pre + 1;
+//}
+
+
+
+//c++用vector的接口做法，但是慢
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		if (nums.empty())
+			return 0;
+
+		vector<int>::iterator pos = nums.begin();
+		vector<int>::iterator cur = nums.begin() + 1;
+		while (cur != nums.end())
 		{
-			pre++;
-			nums[pre] = nums[cur];
+			if (*pos == *cur)
+			{
+				cur = nums.erase(cur);
+				continue;
+			}
+
+			pos++;
 			cur++;
 		}
-	}
 
-	return pre + 1;
-}
+
+		return nums.size();
+	}
+};
