d813837fbb580235dd7cf4685f0caf32acc2b14f
gdis5251 <841784596@qq.com>
2019-04-07 09:50:25 +0800

    [Gerald][update][2019/4/7] add non recursive way

--- 94.二叉树的中序遍历/94.二叉树的中序遍历/Code.h
+++ 94.二叉树的中序遍历/94.二叉树的中序遍历/Code.h
@@ -64,3 +64,40 @@ int* inorderTraversal(struct TreeNode* root, int* returnSize) {
 	*returnSize = size;
 	return array;
 }
+
+//non recursive
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	vector<int> inorderTraversal(TreeNode* root) {
+		vector<int> ans;
+		stack<TreeNode*> s;
+
+		TreeNode *cur = root;
+
+		while (cur || !s.empty())
+		{
+			while (cur)
+			{
+				s.push(cur);
+				cur = cur->left;
+			}
+
+			cur = s.top();
+			ans.push_back(cur->val);
+
+			s.pop();
+			cur = cur->right;
+		}
+
+		return ans;
+	}
+};
