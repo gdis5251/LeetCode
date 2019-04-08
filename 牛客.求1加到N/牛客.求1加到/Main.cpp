eb2b6413a8f23cd8e37ad088fb2225de38e77a89
gdis5251 <841784596@qq.com>
2019-04-08 13:12:29 +0800

    [Gerald][update][2019/4/8] add annotation

--- 牛客.求1加到N/牛客.求1加到/Main.cpp
+++ 牛客.求1加到N/牛客.求1加到/Main.cpp
@@ -12,8 +12,8 @@ public:
 		}
 	};
 	int Sum_Solution(int n) {
-		sum = 0;
-		num = 1;
+		sum = 0;//因为在创建对象的时候会调用默认构造函数，所以这里要再一次的赋值
+		num = 1;//让他们两个变量的值恢复初始状态
 		Sum arr[n];//因为牛客网的编译器是clang,所以支持可变数组
 		return sum;
 	}
