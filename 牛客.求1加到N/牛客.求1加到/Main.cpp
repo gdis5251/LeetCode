c2dfbfa7df77501ff5a97997f09a8d7cfdb478e5
gdis5251 <841784596@qq.com>
2019-04-14 17:02:45 +0800

    [Gerald][update][2019/4/14] 重新理解这道题，之前的理解是错的，具体写在注释里

--- 牛客.求1加到N/牛客.求1加到/Main.cpp
+++ 牛客.求1加到N/牛客.求1加到/Main.cpp
@@ -12,9 +12,9 @@ public:
 		}
 	};
 	int Sum_Solution(int n) {
-		sum = 0;//因为在创建对象的时候会调用默认构造函数，所以这里要再一次的赋值
-		num = 1;//让他们两个变量的值恢复初始状态
-		Sum arr[n];//因为牛客网的编译器是clang,所以支持可变数组
+		sum = 0;//这里进行多次赋值的原因是，因为两个变量都是静态变量，所以他们的生命周期是整个程序的生命周期
+		num = 1;//若这个函数只调用一次没事，如果调用多次，不进行赋值，他们的值不会改变，会导致结果错误
+		Sum arr[5];//因为牛客网的编译器是clang,所以支持可变数组
 		return sum;
 	}
 private:
@@ -25,3 +25,9 @@ private:
 int Solution::sum = 0;
 int Solution::num = 1;
 
+int main(void)
+{
+	Solution a;
+	a.Sum_Solution(5);
+	return 0;
+}
