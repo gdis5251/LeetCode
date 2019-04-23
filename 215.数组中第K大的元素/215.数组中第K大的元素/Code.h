d9883c8f0b78c457acc1f78a1735728076474b4e
gdis5251 <841784596@qq.com>
2019-04-23 22:13:01 +0800

    [Gerald][update][2019/4/23] 优化了算法

--- 215.数组中第K大的元素/215.数组中第K大的元素/Code.h
+++ 215.数组中第K大的元素/215.数组中第K大的元素/Code.h
@@ -16,3 +16,27 @@ public:
 		return pq.top();
 	}
 };
+
+//”≈ªØ
+class Solution {
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+		std::priority_queue<int, std::vector<int>, std::greater<int> > pq;
+
+		for (size_t i = 0; i < k; i++)
+		{
+			pq.push(nums[i]);
+		}
+
+		for (size_t j = k; j < nums.size(); j++)
+		{
+			if (nums[j] > pq.top())
+			{
+				pq.pop();
+				pq.push(nums[j]);
+			}
+		}
+
+		return pq.top();
+	}
+};
