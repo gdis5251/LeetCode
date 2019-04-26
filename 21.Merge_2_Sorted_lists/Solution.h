a1c0ca0ed72777cf5915a01782db2759b9495201
gdis5251 <841784596@qq.com>
2019-04-26 16:08:28 +0800

    [Gerald][update][2019/4/26]  增加非递归版本

--- 21.Merge_2_Sorted_lists/Solution.h
+++ 21.Merge_2_Sorted_lists/Solution.h
@@ -30,3 +30,73 @@ public:
     }
     
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		ListNode *cur1 = l1;
+		ListNode *cur2 = l2;
+		ListNode *dummyHead = new ListNode(0);
+		ListNode *curD = dummyHead;
+
+		while (cur1 && cur2)
+		{
+			if (cur1->val <= cur2->val)
+			{
+				if (dummyHead->next == nullptr)
+				{
+					dummyHead->next = cur1;
+					curD = dummyHead->next;
+
+					cur1 = cur1->next;
+				}
+				else
+				{
+					curD->next = cur1;
+					curD = curD->next;
+
+					cur1 = cur1->next;
+				}
+			}
+			else
+			{
+				if (dummyHead->next == nullptr)
+				{
+					dummyHead->next = cur2;
+					curD = dummyHead->next;
+
+					cur2 = cur2->next;
+				}
+				else
+				{
+					curD->next = cur2;
+					curD = curD->next;
+
+					cur2 = cur2->next;
+				}
+			}
+
+		}
+
+		if (cur1 == nullptr)
+			curD->next = cur2;
+		if (cur2 == nullptr)
+			curD->next = cur1;
+
+		ListNode *ret = dummyHead->next;
+
+		delete dummyHead;
+		dummyHead = nullptr;
+
+		return ret;
+	}
+};
