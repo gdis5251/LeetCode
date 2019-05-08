aced434347c46e1986fb45b45a62e82b070ee090
gdis5251 <841784596@qq.com>
2019-05-08 13:33:48 +0800

    [Gerald][update][2019/5/8] 可以调试

---
+++ 151.翻转字符串里的单词/Code.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    static void reverse(string& s, int lhs, int rhs)
+    {
+        while (lhs < rhs)
+        {
+            swap(s[lhs++], s[rhs--]);
+        }
+    }
+    
+    static string reverseWords(string s) {
+        if (s.empty())
+            return s;
+        
+        std::reverse(s.begin(), s.end());
+        
+        size_t space = 0;
+        int b = 0;
+        int e = -2;
+        bool isContinue = true;
+        while (isContinue && !s.empty())
+        {
+            if (space == 0)
+                b = 0;
+            else
+                b = space;
+            
+            space = s.find(' ', space);
+                
+            if (space == s.npos)
+            {
+                isContinue = false;
+                e = s.size() - 1;
+            }
+            else
+            {
+                e = space - 1;
+            }
+            
+            if (space == 0 || space == s.size() - 1)
+            {
+                s.erase(space, 1);
+                continue;
+            }
+            else if (s[space + 1] == ' ' && s[space - 1] != ' ')
+            {
+                s.erase(space, 1);
+            }
+            else
+            {
+                space++;
+            }
+            
+            reverse(s, b, e);
+        }
+        
+        return s;
+    }
+};
