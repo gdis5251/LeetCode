e2182fabd09d9feec55cc802a6e37702a24b64d7
gdis5251 <841784596@qq.com>
2019-05-23 16:52:58 +0800

    [Gerald][update][2019/5/23] 改进，之前没有输出否定的结果

--- 每日一题/7.计算糖果/Main.cpp
+++ 每日一题/7.计算糖果/Main.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-#include <vector>
-
-int main(void)
-{
-    std::vector<int> input;
-    std::vector<int> output;
-    input.resize(4);
-    output.resize(3);
-    
-    while (std::cin >> input[0] >> input[1] >> input[2] >> input[3])
-    {
-        output[0] = (input[0] + input[2]) / 2;
-        output[1] = (input[1] + input[3]) / 2;
-        output[2] = input[3] - output[1];
-        
-        std::cout << output[0] << " " << output[1] << " " << output[2] << std::endl;
-    }
-    return 0;
+#include<iostream>
+#include<vector>
+#include<cmath>
+#include<algorithm>
+using namespace std;
+ 
+ 
+int main(){
+	int n1, n2, n3, n4;
+	int A, B, C;
+	cin >> n1 >> n2 >> n3 >> n4;
+ 
+	A = (n1 + n3) / 2;
+	B = A - n1;
+	C = B - n2;
+	if (A >= 0 && B >= 0 && C >= 0 && n4 == (B + C))
+		cout << A << " " << B << " " << C;
+	else
+		cout << "No";
+ 
+	
 }
