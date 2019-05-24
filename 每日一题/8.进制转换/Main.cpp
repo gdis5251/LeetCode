994affb8d41d87bef9498d22653a0c04bac6a7ff
gdis5251 <841784596@qq.com>
2019-05-24 22:53:25 +0800

    [Gerald][update][2019/5/24] 更新处理负数的情况

--- 每日一题/8.进制转换/Main.cpp
+++ 每日一题/8.进制转换/Main.cpp
@@ -3,12 +3,14 @@
 
 int main(void)
 {
-    std::vector<unsigned int> input;
+    std::vector<int> input;
     input.resize(2);
     std::vector<char> table = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'E', 'F'};
+    int flag = 0;
     
     while (std::cin >> input[0] >> input[1])
     {
+        flag = 0;
         std::vector<char> ans;
        
         if (input[0] == 0)
@@ -18,12 +20,19 @@ int main(void)
         }    
 
         if (input[0] < 0) // 如果是负数，就把这个数变成补码
-            input[0] = ~input[0] + 1;
+        {
+            input[0] = -input[0];
+            flag = 1;
+        }
 
         while (input[0] != 0)
         {
             ans.push_back(table[input[0] % input[1]]);
             input[0] /= input[1];
+        }
+        if (flag == 1)
+        {
+            ans.push_back('-');
         }
         
         for (int i = static_cast<int>(ans.size()) - 1; i >= 0; i--)
