1cdb4d1ff45fb3a21da8df69239118f8fd89b5fd
gdis5251 <841784596@qq.com>
2019-07-21 15:00:31 +0800

    [Gerald][write][2019/7/21] 动态规划，除第一行和第一列都为1外，其余个点的方法都等于它的上面的方法加上它的左边的方法

---
+++ 牛客.unique_paths/Code.hpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        std::vector<std::vector<int> > dp(m, std::vector<int>(n, 0));
+        dp[0][0] = 1;
+
+        // 第一行全部为1
+        for (int i = 1; i < n; i++)
+        {
+            dp[0][i] = 1;
+        }
+        // 第一列全部为1
+        for (int j = 1; j < m; j++)
+        {
+            dp[j][0] = 1;
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+};
+
