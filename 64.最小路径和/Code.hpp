13509766616c7486e410130bad6a030e047cc965
gdis5251 <841784596@qq.com>
2019-07-22 17:36:54 +0800

    [Gerald][write][2019/7/22] 基本掌握该方法

---
+++ 64.最小路径和/Code.hpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        if (grid.empty())
+        {
+            return 0;
+
+        }
+
+        int row = grid.size();
+        int col = grid[0].size();
+
+        std::vector<std::vector<int>> dp(row, std::vector<int>(col, 0));
+        dp[0][0] = grid[0][0];
+
+        // 第一行
+        for (int i = 1; i < col; i++)
+        {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+
+        // 第一列
+        for (int j = 1; j < row; j++)
+        {
+            dp[j][0] = dp[j - 1][0] + grid[j][0];
+        }
+
+        for (int i = 1; i < row; i++)
+        {
+            for (int j = 1; j < col; j++)
+            {
+                dp[i][j] = std::min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return dp[row - 1][col - 1];
+
+    }
+};
+
