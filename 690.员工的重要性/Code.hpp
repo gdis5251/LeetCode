de902a597875a6a01ff9a76600b0ebac38083978
gdis5251 <841784596@qq.com>
2019-07-30 10:05:19 +0800

    [Gerald][update][2019/7/30] Add bfs

--- 690.员工的重要性/Code.hpp
+++ 690.员工的重要性/Code.hpp
@@ -47,3 +47,59 @@ public:
     }
 
 };
+
+
+
+// bfs
+/*
+   Employee info
+   class Employee {
+   public:
+// It's the unique ID of each node.
+// unique id of this employee
+int id;
+// the importance value of this employee
+int importance;
+// the id of direct subordinates
+vector<int> subordinates;
+
+};
+*/
+class Solution {
+public:
+
+    int getImportance(vector<Employee*> employees, int id) {
+        // 存储 id 
+        queue<int> q;
+        int ans = 0;
+
+        std::unordered_map<int, Employee*> info;
+        for (auto e : employees)
+        {
+            info[e->id] = e;
+        }
+
+        q.push(id);
+        while (!q.empty())
+        {
+            int tmp = q.front();
+            q.pop();
+
+            ans += info[tmp]->importance;
+
+            for (auto e : info[tmp]->subordinates)
+            {
+                q.push(e);
+            }
+        }
+
+        return ans;
+    }
+
+
+};
+}
+}
+}
+}
+}
